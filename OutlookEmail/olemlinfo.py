02caff817917dd84ca15c43dd7453d0f777a3bda
Hector Quemada <44162051+hdquemada@users.noreply.github.com>
2020-05-27 10:39:54 -0400

    Delete olemlinfo.py

--- OutlookEmail/olemlinfo.py
+++
@@ -1,8 +0,0 @@
-# YouTube Video: https://www.youtube.com/watch?v=mP_Ln-Z9-XY
-# Login informaton for python email script "newtest_email.py" and "mwc_email2.py"
-
-EMAIL_ADDRESS = 'hquemada@wmich.edu'
-PASSWORD = 'TheN3xtD@yAt1'
-SUBJECT = 'Test Email'
-EMAIL_SEND = 'hdquemada@gmail.com', 'hdquemada@yahoo.com', 'hector.quemada@wmich.edu'
-BODY = 'Test body'
