80b89603c3861f2d245a5d42063c40f9083f871b
Hector Quemada <44162051+hdquemada@users.noreply.github.com>
2020-05-27 10:39:22 -0400

    Delete emlinfo.py

--- MWCEmail/emlinfo.py
+++
@@ -1,8 +0,0 @@
-# YouTube Video: https://www.youtube.com/watch?v=mP_Ln-Z9-XY
-# Login informaton for python email script "newtest_email.py" and "mwc_email2.py"
-
-EMAIL_ADDRESS = "hdquemada@gmail.com"
-PASSWORD = "tdpmtxigzdqzcrez"
-SUBJECT = 'Ministry With Community reminder'
-EMAIL_SEND = 'hdquemada@gmail.com', 'hdquemada@yahoo.com', 'Rlbush37@gmail.com', 'susankcmi@aol.com', 'susan.dame@wmich.edu', 'dw-mefischer@sbcglobal.net', 'fischer.maryellen@gmail.com', 'quiltkazoo@sbcglobal.net', 'amgilljr@gmail.com', 'JEKlok@sbcglobal.net', 'msodutch@aol.com', 'momzrule3@gmail.com', 'janage@aol.com', 'dwassink@hotmail.com', 'bswassink@yahoo.com', 'Jwylie16awesome2014@gmail.com', 'willsonj@wmis.net'
-BODY = 'Dear Ministry with Community volunteers,\n\n This Saturday is the fourth Saturday of the month, and our opportunity again to serve at Ministry with Community. I hope to see you there at 7:30 AM.\n\n Thanks again for your gift of time and effort to this ministry.\n\n Hector'
