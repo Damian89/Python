0d8a7cb572a93e2ed08dfab698b56a6cdef89847
hdquemada <hdquemada@gmail.com>
2022-12-17 07:34:09 -0500

    update python

---
+++ boxplot/boxplot2.py
@@ -0,0 +1,22 @@
+import plotly.graph_objects as go
+import pandas as pd
+from plotly.graph_objs import Figure
+
+df = pd.read_csv('wdmonthname.csv')
+
+fig: Figure = go.Figure()
+
+fig.add_trace(go.Violin(x=df['Month'], y=df['Weight'],
+                        line_color='rgba(255,0,0,0.5)',
+                        )
+              )
+
+fig.update_traces(box_visible=True, meanline_visible=True,
+                  points='all', pointpos=-0, jitter=0.5,
+                  marker_line_color='rgba(0,0,0,0.5)',
+                  marker_line_width=1,
+                  showlegend=False)
+
+fig.update_layout(template='simple_white')
+
+fig.show()
