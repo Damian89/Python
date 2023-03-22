51c96f9da7cfcc8416eb923b6618dadf890061bf
Hector Quemada <44162051+hdquemada@users.noreply.github.com>
2023-03-22 14:37:58 -0400

    update python

--- boxplot/boxplot.py
+++ boxplot/boxplot.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 df = pd.read_csv('wdmonthname.csv')
 fig = px.violin(df, x="Month", y="Weight", box=True, points='all', title="My Weight Each Month, beginning February 2022")  # add point="all" to show all points.
-fig.show()
+fig.show(renderer='browser')
